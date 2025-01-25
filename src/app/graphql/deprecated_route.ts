5686fc550bffaf54b602eddfcb8e59d77db26491
chauluu <chau96cc@gmail.com>
2025-01-26 01:34:31 +0700

    chore: remove graphql implementation

---
+++ src/app/graphql/deprecated_route.ts
@@ -0,0 +1,26 @@
+// import { ApolloServer } from "@apollo/server";
+// import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
+// import typeDefs from './schema'
+// import resolvers from './resolver'
+// import { NextRequest } from "next/server";
+// import { startServerAndCreateNextHandler } from '@as-integrations/next'
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [
+//         (process.env.NODE_ENV === 'production') ? ApolloServerPluginLandingPageProductionDefault() :
+//             ApolloServerPluginLandingPageLocalDefault({ footer: false, embed: true })
+//     ],
+// });
+
+// const handler = startServerAndCreateNextHandler<NextRequest>(server, {})
+
+// export async function GET(request: NextRequest) {
+//     return handler(request)
+// }
+
+// export async function POST(request: NextRequest) {
+//     console.log('request',request)
+//     return handler(request)
+// }
--- src/app/graphql/schema.ts
+++ src/app/graphql/schema.ts
@@ -1,22 +1,22 @@
-import { gql } from "@apollo/client";
+// import { gql } from "@apollo/client";
 
-const typeDefs = gql`
-  type Book {
-    id: ID!
-    title: String!
-    author: String!
-  }
+// const typeDefs = gql`
+//   type Book {
+//     id: ID!
+//     title: String!
+//     author: String!
+//   }
 
-  type Query {
-    books: [Book]
-    book(id: ID!): Book
-  }
+//   type Query {
+//     books: [Book]
+//     book(id: ID!): Book
+//   }
 
-  type Mutation {
-    createBook(title: String!, author: String!): Book
-    updateBook(id: ID!, title: String, author: String): Book
-    deleteBook(id: ID!): Boolean
-  }
-`;
+//   type Mutation {
+//     createBook(title: String!, author: String!): Book
+//     updateBook(id: ID!, title: String, author: String): Book
+//     deleteBook(id: ID!): Boolean
+//   }
+// `;
 
-export default typeDefs
+// export default typeDefs
